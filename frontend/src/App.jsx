a7b7126783e81dde5f2bc473b98c4da548273235
Abhishek Gupta <guptaabhishek60420@gmail.com>
2026-05-26 21:40:00 +0530

    Improved UI

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 
 function App() {
+
   const [records, setRecords] = useState([]);
   const [file, setFile] = useState(null);
 
@@ -12,23 +13,28 @@ function App() {
   }, []);
 
   const fetchData = async () => {
+
     try {
+
       const response = await axios.get(
         "http://127.0.0.1:8000/api/emissions/"
       );
 
       setRecords(response.data);
+
     } catch (error) {
       console.log(error);
     }
   };
 
   const uploadFile = async () => {
+
     const formData = new FormData();
 
     formData.append("file", file);
 
     try {
+
       await axios.post(
         "http://127.0.0.1:8000/api/upload/",
         formData
@@ -36,14 +42,17 @@ function App() {
 
       fetchData();
 
-      alert("File uploaded successfully");
+      alert("CSV Uploaded Successfully");
+
     } catch (error) {
       console.log(error);
     }
   };
 
   const approveRecord = async (id) => {
+
     try {
+
       await axios.post(
         `http://127.0.0.1:8000/api/approve/${id}/`
       );
@@ -54,7 +63,9 @@ function App() {
       console.log(error);
     }
   };
+
   const rejectRecord = async (id) => {
+
     try {
 
       await axios.post(
@@ -71,11 +82,11 @@ function App() {
   const filteredRecords = records.filter((record) => {
 
     if (filter === "SUSPICIOUS") {
-      return record.is_suspicious === true;
+      return record.is_suspicious;
     }
 
     if (filter === "APPROVED") {
-      return record.approved === true;
+      return record.approved;
     }
 
     return true;
@@ -90,104 +101,258 @@ function App() {
   ).length;
 
   return (
-    <div style={{ padding: "20px" }}>
 
-      <h1>Emission Review Dashboard</h1>
+  <div className="min-h-screen bg-gray-100 p-8 text-black">
 
-      <br />
+    {/* HEADER */}
 
-      <input
-        type="file"
-        onChange={(e) => setFile(e.target.files[0])}
-      />
+    <div className="mb-8">
 
-      <button onClick={uploadFile}>
-        Upload CSV
-      </button>
+      <h1 className="text-4xl font-bold text-black">
+        ESG Emission Review Dashboard
+      </h1>
 
-      <hr />
+      <p className="text-gray-600 mt-2">
+        Review, approve and manage emission records
+      </p>
 
-      <h2>Summary</h2>
+    </div>
 
-      <p>Total Records: {records.length}</p>
+    {/* TOP CARDS */}
 
-      <p>Suspicious Records: {suspiciousCount}</p>
+    <div className="grid grid-cols-4 gap-6 mb-8">
 
-      <p>Approved Records: {approvedCount}</p>
+      {/* UPLOAD */}
 
-      <hr />
+      <div className="bg-white rounded-2xl p-6 shadow">
 
-      <button onClick={() => setFilter("ALL")}>
+        <h2 style={{
+            fontSize: "19px",
+            fontWeight: "bold",
+            color: "#364153",
+            marginTop: "16px",
+          }}>
+          Upload CSV File
+        </h2>
+
+        <input
+          type="file"
+          onChange={(e) =>
+            setFile(e.target.files[0])
+          }
+          className="w-full border p-3 rounded-lg text-black"
+        />
+
+        <button
+          onClick={uploadFile}
+          className="mt-4 w-full bg-black text-white py-3 rounded-xl font-semibold hover:bg-gray-800 transition"
+        >
+          Upload CSV
+        </button>
+
+      </div>
+
+      {/* TOTAL */}
+
+      <div className="bg-blue-100 rounded-2xl p-6 shadow flex flex-col justify-center items-center">
+
+        <p className="text-gray-700 text-lg">
+          Total Records
+        </p>
+
+        <h2
+          style={{
+            fontSize: "40px",
+            fontWeight: "bold",
+            color: "#2563eb",
+            marginTop: "16px",
+          }}
+        >
+          {records.length}
+        </h2>
+
+      </div>
+
+      {/* APPROVED */}
+
+      <div className="bg-green-100 rounded-2xl p-6 shadow flex flex-col justify-center items-center">
+
+        <p className="text-green-700 text-lg">
+          Approved
+        </p>
+
+        <h2
+          style={{
+            fontSize: "40px",
+            fontWeight: "bold",
+            color: "#15803d",
+            marginTop: "16px",
+          }}
+        >
+          {approvedCount}
+        </h2>
+
+      </div>
+
+      {/* SUSPICIOUS */}
+      <div className="bg-red-100 rounded-2xl p-6 shadow flex flex-col justify-center items-center">
+
+        <p className="text-red-700 text-lg">
+          Suspicious Records
+        </p>
+
+        <h2
+          style={{
+            fontSize: "40px",
+            fontWeight: "bold",
+            color: "#b91c1c",
+            marginTop: "16px",
+          }}
+        >
+          {suspiciousCount}
+        </h2>
+      </div>
+    </div>
+
+    {/* FILTERS */}
+
+    <div className="flex gap-4 mb-6">
+
+      <button
+        onClick={() => setFilter("ALL")}
+        className="bg-black text-white px-5 py-2 rounded-xl"
+      >
         All
       </button>
 
-      <button onClick={() => setFilter("SUSPICIOUS")}>
+      <button
+        onClick={() => setFilter("SUSPICIOUS")}
+        className="bg-red-500 text-white px-5 py-2 rounded-xl"
+      >
         Suspicious
       </button>
 
-      <button onClick={() => setFilter("APPROVED")}>
+      <button
+        onClick={() => setFilter("APPROVED")}
+        className="bg-green-500 text-white px-5 py-2 rounded-xl"
+      >
         Approved
       </button>
 
-      <br />
-      <br />
+    </div>
 
-      <table
-        border="1"
-        cellPadding="10"
-        style={{
-          borderCollapse: "collapse",
-          width: "100%",
-        }}
-      >
-        <thead>
+    {/* TABLE */}
+
+    <div className="bg-white rounded-2xl shadow overflow-hidden">
+
+      <table className="w-full">
+
+        <thead className="bg-gray-200">
+
           <tr>
-            <th>ID</th>
-            <th>Source</th>
-            <th>Category</th>
-            <th>Value</th>
-            <th>Unit</th>
-            <th>Status</th>
-            <th>Approved</th>
-            <th>Action</th>
+
+            <th className="p-4 text-left">
+              ID
+            </th>
+
+            <th className="p-4 text-left">
+              Source
+            </th>
+
+            <th className="p-4 text-left">
+              Category
+            </th>
+
+            <th className="p-4 text-left">
+              Value
+            </th>
+
+            <th className="p-4 text-left">
+              Unit
+            </th>
+
+            <th className="p-4 text-left">
+              Status
+            </th>
+
+            <th className="p-4 text-left">
+              Action
+            </th>
+
           </tr>
+
         </thead>
 
         <tbody>
+
           {filteredRecords.map((record) => (
+
             <tr
               key={record.id}
-              style={{
-                backgroundColor: record.is_suspicious
-                  ? "#ffcccc"
+              className={`border-t hover:bg-gray-50 ${
+                record.is_suspicious
+                  ? "bg-red-50"
                   : record.approved
-                  ? "#ccffcc"
-                  : "#ffffff",
-              }}
+                  ? "bg-green-50"
+                  : ""
+              }`}
             >
-              <td>{record.id}</td>
 
-              <td>{record.source}</td>
-
-              <td>{record.category}</td>
-
-              <td>{record.raw_value}</td>
-
-              <td>{record.raw_unit}</td>
-
-              <td>{record.status}</td>
-
-              <td>
-                {record.approved ? "YES" : "NO"}
+              <td className="p-4">
+                {record.id}
               </td>
 
-              <td>
-                {!record.approved && (
+              <td className="p-4 font-semibold">
+                {record.source}
+              </td>
+
+              <td className="p-4">
+                {record.category}
+              </td>
+
+              <td className="p-4">
+                {record.raw_value}
+              </td>
+
+              <td className="p-4">
+                {record.raw_unit}
+              </td>
+
+              {/* STATUS */}
+
+              <td className="p-4">
+
+                <span
+                  className={`px-3 py-1 rounded-full text-white text-sm font-semibold
+                  ${
+                    record.status === "APPROVED"
+                      ? "bg-green-500"
+                      : record.status === "REVIEW"
+                      ? "bg-yellow-500"
+                      : record.status === "FAILED"
+                      ? "bg-red-500"
+                      : "bg-blue-500"
+                  }`}
+                >
+                  {record.status}
+                </span>
+
+              </td>
+
+              {/* ACTIONS */}
+
+              <td className="p-4 flex gap-3">
+
+                {record.status !== "APPROVED" &&
+                  record.status !== "FAILED" && (
+
                   <>
+
                     <button
                       onClick={() =>
                         approveRecord(record.id)
                       }
+                      className="bg-green-500 hover:bg-green-600 text-white px-4 py-2 rounded-lg"
                     >
                       Approve
                     </button>
@@ -196,22 +361,29 @@ function App() {
                       onClick={() =>
                         rejectRecord(record.id)
                       }
-                      style={{
-                        marginLeft: "10px",
-                      }}
+                      className="bg-red-500 hover:bg-red-600 text-white px-4 py-2 rounded-lg"
                     >
                       Reject
                     </button>
+
                   </>
+
                 )}
+
               </td>
+
             </tr>
+
           ))}
+
         </tbody>
+
       </table>
 
     </div>
-  );
+
+  </div>
+);
 }
 
 export default App;
